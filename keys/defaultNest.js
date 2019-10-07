73045597c55863ad12c939cef20a5b03cf5dec48
MrRecordHolder <pokecloudbot@gmail.com>
2019-10-07 00:34:58 -0400

    master default key dataset

---
+++ keys/defaultNest.js
@@ -0,0 +1,39 @@
+const defaultNest = {
+    name: "",
+    alias: "",
+    serverid: "",
+    location: {
+        city: "",
+        state: "",
+        country: "",
+        region: "",
+        maps: {
+            lat: "",
+            lon: "",
+            google: "",
+            osm: "",
+            silphroad: ""
+        }
+    },
+    pokestops: "?",
+    gyms: "?",
+    exgyms: "0",
+    spawns: "?",
+    pokemon: {
+        current: {
+            name: "?",
+            image: "https://github.com/MrRecordHolder/pokecloud/blob/master/images/pokemon/unreported.png?raw=true"
+        },
+        previous: {
+            name: "?",
+            image: "https://github.com/MrRecordHolder/pokecloud/blob/master/images/pokemon/unreported.png?raw=true"
+        }
+    },
+    messageid: "",
+    servershare: {
+        serverid: "",
+        messageid: ""
+    }
+}
+
+module.exports = defaultNest
---
+++ keys/guildSettings.js
@@ -0,0 +1,46 @@
+const guildSettings = {
+    name: "",
+    id: "",
+    owner: "",
+    language: "English",
+    prefix: "$",
+    location: {
+        city: "",
+        state: "",
+        country: "",
+        region: "",
+        timezone: "",
+        maps: {
+            tsr: ""
+        }
+    },
+    channels: {
+        admin: "",
+        nest: "",
+        log: ""
+    },
+    roles: {
+        admin: "",
+        nest: ""
+    },
+    bots: {
+        pokenav: false,
+        miscord: false
+    },
+    clean: {
+        commands: true,
+        replies: true,
+    },
+    migration: {
+        tag: false,
+        tagrole: "",
+        tagchannel: "",
+        messageid: "",
+        dates: {
+            last: "",
+            next: ""
+        }
+    }
+}
+
+module.exports = guildSettings

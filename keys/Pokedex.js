264d38f9adab23b6c672bf8eaff6c3504544a965
MrRecordHolder <pokecloudbot@gmail.com>
2019-10-30 17:05:17 -0400

    NEW pokedex upgrade

---
+++ keys/Pokedex.js
@@ -0,0 +1,21 @@
+const Pokedex = {
+    name: {
+        english: "",
+        german: "",
+        french: "",
+        japanese: ""
+    },
+    dex: "",
+    shiny: false,
+    nest: true,
+    type: {
+        primary: "",
+        secondary: ""
+    },
+    boost: {
+        primary: "",
+        secondary: ""
+    }
+};
+
+module.exports = Pokedex

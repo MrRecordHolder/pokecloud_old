0b3466c7a5bbb1e8a6764beed2ad961fdd5e4f50
MrRecordHolder <pokecloudbot@gmail.com>
2019-11-03 20:49:25 -0500

    bug fix

--- commands/pokedex.js
+++ commands/pokedex.js
@@ -10,19 +10,16 @@ module.exports.command = {
 }
 
 const Discord = require("discord.js")
-const errors = require("../util/data/errors.json")
-const times = require("../util/data/times.json")
-const Pokedex = require("../keys/Pokedex")
 
 exports.run = (bot, message, args) => { 
     let responseObject = bot.Pokedex.findAll(`nest`, true)
-    let pokemon = responseObject.map(key => [`${key.english.name}`])
+    let pokemon = responseObject.map(key => [`${key.name.english}`])
     
     var embed = new Discord.RichEmbed()
         .setColor("RANDOM")
         .setTitle(`**Nesting Pokemon**`)    
         .setThumbnail(`https://github.com/MrRecordHolder/pokecloud/blob/master/images/emojis/spawn.png?raw=true`)
         .setDescription(pokemon.sort().slice(0, 50))
-        .setFooter('Data provide by PokeCloud', bot.user.avatarURL)
+        .setFooter('Data provide by PokeCloud')
     message.channel.send({embed});
 }

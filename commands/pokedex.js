cdd08a91b557c2a3544e1fa3f884709d27fa5951
MrRecordHolder <pokecloudbot@gmail.com>
2019-11-06 01:00:15 -0500

    gen sort

--- commands/pokedex.js
+++ commands/pokedex.js
@@ -20,10 +20,9 @@ exports.run = (bot, message, args) => {
 
     if(!output[0]) {
         let pokemon = bot.Pokedex.map(key => [`${key.name.english}`])
-        
         var embed = new Discord.RichEmbed()
             .setColor("RANDOM")
-            .setTitle(`**All Pokemon**`)    
+            .setTitle(`**All Species**`)    
             .setThumbnail(`https://github.com/MrRecordHolder/pokecloud/blob/master/images/emojis/spawn.png?raw=true`)
             .setDescription(pokemon.sort().join(", "))
             .setFooter('Data provide by PokeCloud')
@@ -31,15 +30,53 @@ exports.run = (bot, message, args) => {
     }
 
     if(output[0].trim().toLowerCase() === "nest") {
-        let responseObject = bot.Pokedex.findAll(`nest`, true)
-        let pokemon = responseObject.map(key => [`${key.name.english}`])
+        if(output[1]) {
+            if(output[1].trim() === "shiny") {
+                let responseObject = bot.Pokedex.findAll(`nest`, true)
+                let responseObject_nest = responseObject.filter(v => v.nest === true);
+                let responseObject_shiny = responseObject_nest.filter(v => v.shiny === true)
+                let pokemon = responseObject_shiny.map(key => [`${key.name.english}`])
+                var embed = new Discord.RichEmbed()
+                    .setColor("RANDOM")
+                    .setTitle(`**Nesting Species (Shiny)**`)
+                    .setThumbnail(`https://github.com/MrRecordHolder/pokecloud/blob/master/images/emojis/spawn.png?raw=true`)
+                    .setDescription(pokemon.sort().join(", "))
+                    .setFooter('Data provide by PokeCloud')
+                return message.channel.send({embed});
+            };
+        };
         
+        let responseObject = bot.Pokedex.findAll(`nest`, true)
+
+        let gen_i = responseObject.filter(v => v.dex <= 151);
+        let pokemon_gen_i = gen_i.map(key => [`${key.name.english}`])
+
+        let gen_ii = responseObject.filter(v => v.dex <= 251);
+        let gen_ii_new = gen_ii.filter(v => v.dex >= 152);
+        let pokemon_gen_ii = gen_ii_new.map(key => [`${key.name.english}`])
+
+        let gen_iii = responseObject.filter(v => v.dex <= 386);
+        let gen_iii_new = responseObject.filter(v => v.dex >= 252);
+        let pokemon_gen_iii = gen_iii_new.map(key => [`${key.name.english}`])
+
+        let gen_iv = responseObject.filter(v => v.dex <= 491);
+        let gen_iv_new = responseObject.filter(v => v.dex >= 387);
+        let pokemon_gen_iv = gen_iv_new.map(key => [`${key.name.english}`])
+
+        let gen_v = responseObject.filter(v => v.dex <= 631);
+        let gen_v_new = responseObject.filter(v => v.dex >= 492);
+        let pokemon_gen_v = gen_v_new.map(key => [`${key.name.english}`])
+
         var embed = new Discord.RichEmbed()
             .setColor("RANDOM")
-            .setTitle(`**Nesting Pokemon**`)
+            .setTitle(`**Nesting Species**`)
             .setThumbnail(`https://github.com/MrRecordHolder/pokecloud/blob/master/images/emojis/spawn.png?raw=true`)
-            .setDescription(pokemon.sort().join(", "))
-            .setFooter('Data provide by PokeCloud')
+            .addField("**Gen 1 - Kanto**", pokemon_gen_i.sort().join(", "))
+            .addField("**Gen 2 - Johto**", pokemon_gen_ii.sort().join(", "))
+            .addField("**Gen 3 - Hoenn**", pokemon_gen_iii.sort().join(", "))
+            .addField("**Gen 4 - Sinnoh**", pokemon_gen_iv.sort().join(", "))
+            .addField("**Gen 5 - Unova**", pokemon_gen_v.sort().join(", "))
+            .setFooter('Data provide by PokeCloud');
         message.channel.send({embed});
     };        
 }
